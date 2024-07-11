3f6a35165f7007d328007a5d6ec8f808059e7d10
hwalker928 <harry@harryw.link>
2024-07-11 16:38:00 +0100

    black

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import mido
 
+
 def debug():
     print("\nisChannelMuted", isChannelMuted)
-    print("micLive",micLive)
-    print("channelVolumeLevels", channelVolumeLevels,"\n")
+    print("micLive", micLive)
+    print("channelVolumeLevels", channelVolumeLevels, "\n")
+
 
 input("Is it set to default? ")
 
@@ -24,46 +26,24 @@ with mido.open_input(input_port_name) as input_port:
     print(f"Listening on {input_port_name}...")
 
     # Continuously read and print MIDI messages
-    isChannelMuted = [
-        True,
-        True,
-        True,
-        True,
-        True,
-        True
-    ]
+    isChannelMuted = [True, True, True, True, True, True]
 
-    channelVolumeLevels = [
-        0,
-        0,
-        0,
-        0,
-        0,
-        0
-    ]
+    channelVolumeLevels = [0, 0, 0, 0, 0, 0]
 
-    micLive = [
-        False,
-        False,
-        False,
-        False,
-        False,
-        False
-    ]
+    micLive = [False, False, False, False, False, False]
     for message in input_port:
-        if message.control == 27 and message.value == 1: #Mute button
+        if message.control == 27 and message.value == 1:  # Mute button
             isChannelMuted[message.channel] = not isChannelMuted[message.channel]
 
             if channelVolumeLevels[message.channel] > 1:
                 micLive[message.channel] = not isChannelMuted[message.channel]
 
-
-        if message.control == 15: #volume change
+        if message.control == 15:  # volume change
             channelVolumeLevels[message.channel] = message.value
 
             if message.value > 1:
                 micLive[message.channel] = not isChannelMuted[message.channel]
             else:
                 micLive[message.channel] = False
-    
+
         debug()
